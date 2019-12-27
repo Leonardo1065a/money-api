3499a8eda48b0f1dc14e9128f3ee5a5a3365d25b
Leonardo Oliveira <leonardoplxy@gmail.com>
2019-12-27 17:23:45 -0300

    alterando o nome dos model tinhando os '_'

--- src/main/java/com/example/algamoney/api/model/Permissao1.java
+++ src/main/java/com/example/algamoney/api/model/Permissao1.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Permissao.class)
-public abstract class Permissao_ {
+public abstract class Permissao1 {
 
 	public static volatile SingularAttribute<Permissao, Long> codigo;
 	public static volatile SingularAttribute<Permissao, String> descricao;
